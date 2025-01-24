93071f0c3214c11256b308a3665b9cd55bd10869
Simon <simon21.blum@gmail.com>
2025-01-24 09:52:52 +0100

    chore: update shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
-}
-
-export function sanity_check(a: number, b: number): number {
-	return a + b;
 }
